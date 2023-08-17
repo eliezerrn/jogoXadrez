f94e586d22bcdfc294d162f7102ee96560af7b76
eliezerrn <eliezerrn@gmail.com>
2023-08-16 21:53:03 -0300

    Classe posição no pacote tabuleiro

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,9 +1,14 @@
 package Aplicacao;
 
+import Tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !");
+		
+		Posicao p = new Posicao(3,5);
+	
+		System.out.println("posição " + p);
 
 	}
 
---
+++ src/Tabuleiro/Posicao.java
@@ -0,0 +1,34 @@
+package Tabuleiro;
+
+public class Posicao {
+
+	private int coluna;
+	private int linha;
+	
+	public Posicao(int coluna, int linha) {	
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+	
+	@Override
+	public String toString() {
+		return linha + ", " + coluna;
+	}
+	
+}
