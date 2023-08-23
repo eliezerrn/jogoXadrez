141ba167399aabd0bb0add9181d23746ecc6aca3
Eliezer Roger Nardi <eliezerrn@gmail.com>
2023-08-23 13:24:39 -0300

    adicionado movimentos do rei

--- src/Xadrez/pecas/Rei.java
+++ src/Xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package Xadrez.pecas;
 
+import MesaDoJogo.Posicao;
 import MesaDoJogo.Tabuleiro;
 import Xadrez.Cor;
 import Xadrez.XadrezPeca;
@@ -15,10 +16,67 @@ public class Rei extends XadrezPeca {
 	public String toString() {
 		return "R";
 	}
+	
+	private boolean podeMover(Posicao posicao) {
+		XadrezPeca p = (XadrezPeca) getTabuleiro().peca(posicao);
+		return p == null || p.getCor() != getCor();
+	}
 
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] matriz = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao(0, 0);
+		
+		//Acima
+		p.setValores(posicao.getLinha() - 1 , posicao.getColuna());
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Abaixo
+		p.setValores(posicao.getLinha() + 1 , posicao.getColuna());
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Esquerda
+		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Direita
+		p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Noroeste
+		p.setValores(posicao.getLinha() -1 , posicao.getColuna() - 1);
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Noroeste
+		p.setValores(posicao.getLinha() -1 , posicao.getColuna() + 1);
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Suldoeste
+		p.setValores(posicao.getLinha() +1 , posicao.getColuna() - 1);
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Suldeste
+		p.setValores(posicao.getLinha() +1 , posicao.getColuna() + 1);
+		if(getTabuleiro().isPosicaoExiste(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		
 		
 		return matriz;
 	}
